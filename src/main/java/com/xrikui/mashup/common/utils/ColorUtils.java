1ec53543e33221eaf4331b2e1005a102f62cef9e
七曦 <koalacute@aliyun.com>
2020-08-16 14:35:13 +0800

    优化代码 && 增加非交易时间判断

---
+++ src/main/java/com/xrikui/mashup/common/utils/ColorUtils.java
@@ -0,0 +1,14 @@
+package com.xrikui.mashup.common.utils;
+
+public class ColorUtils {
+
+    /**
+     * 红色
+     */
+    public final static String RED = "#FF0000";
+
+    /**
+     * 深蓝色
+     */
+    public final static String BLUE_PRO = "#173177";
+}
--- src/main/java/com/xrikui/mashup/common/utils/DateTimeUtils.java
+++ src/main/java/com/xrikui/mashup/common/utils/DateTimeUtils.java
@@ -1,8 +1,14 @@
 package com.xrikui.mashup.common.utils;
 
+import java.time.LocalTime;
+
 public class DateTimeUtils {
 
     public static String YYYY_MM_DD = "yyyyMMdd";
 
     public static String YYYY_MM_DD_HH_MM_SS = "yyyy年MM月dd日 HH:mm:ss";
+
+    public static boolean checkTime() {
+        return LocalTime.now().isAfter(LocalTime.of(9, 29, 59)) && LocalTime.now().isBefore(LocalTime.of(15, 0, 1));
+    }
 }
