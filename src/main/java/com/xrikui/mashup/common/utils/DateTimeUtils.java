bae26739e4ce2178494bf8f33c6556f0f51eb52c
七曦 <koalacute@aliyun.com>
2020-08-18 19:47:10 +0800

    新分支:狗总定制化需求,去掉所有中文日志和信息

--- src/main/java/com/xrikui/mashup/common/utils/DateTimeUtils.java
+++ src/main/java/com/xrikui/mashup/common/utils/DateTimeUtils.java
@@ -8,6 +8,8 @@ public class DateTimeUtils {
 
     public static String YYYY_MM_DD_HH_MM_SS = "yyyy年MM月dd日 HH:mm:ss";
 
+    public static String YYYY_MM_DD_HH_MM_SS_E = "yyyy:MM:dd HH:mm:ss";
+
     public static boolean checkTime() {
         return LocalTime.now().isAfter(LocalTime.of(9, 29, 59)) && LocalTime.now().isBefore(LocalTime.of(15, 0, 1));
     }
